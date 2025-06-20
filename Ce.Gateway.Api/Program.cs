4bb24c0b1127a957e79ba4dc0266b75e57d588b2
Huy Đinh <dinhquanghuy.work@gmail.com>
2025-06-20 16:33:42 +0700

    thêm thư viện Polly để setting timeout cho từng request và them serilog

--- Ce.Gateway.Api/Program.cs
+++ Ce.Gateway.Api/Program.cs
@@ -1,26 +1,52 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
 
 namespace Ce.Gateway.Api
 {
     public class Program
     {
+        public IConfiguration Configuration { get; }
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up Ce.Gateway.Api");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+            
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() // Use Serilog for logging
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
                 .ConfigureAppConfiguration((hostContext, config) =>
                 {
-                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-                    config.AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
-                        .AddJsonFile($"configuration.{env}.json", optional: true, reloadOnChange: true)
-                        .AddEnvironmentVariables();
-                });
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            config.AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"configuration.{env}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        });
     }
 }
